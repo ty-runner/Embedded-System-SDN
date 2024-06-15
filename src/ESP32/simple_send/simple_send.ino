f27d77445a30f96b10867df25241163140722e80
ty-runner <ty.runner02@gmail.com>
2024-06-14 21:41:30 -0700

    Remote setup

---
+++ src/ESP32/simple_send/simple_send.ino
@@ -0,0 +1,47 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "DHT.h"
+
+#define DHTPIN 23
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid = "MountVernon";
+const char* password = "DG860A9EC502";
+const char* serverName = "http://0.0.0.0:5000/";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  dht.begin();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    temperature = temperature * 1.8 + 32;
+    String postData = "temperature=" + String(temperature) + "&humidity=" + String(humidity);
+    int httpResponseCode = http.POST(postData);
+    
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+  }
+  delay(6000); // Send data every minute
+}
